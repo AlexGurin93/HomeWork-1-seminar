dbba4f8f22224ca1cdc33d238d2db4a2096c22ea
Alexander <rold_teem_dfc@mail.ru>
2022-07-10 18:10:56 +0300

    Dz

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿   //     int a, b;
+ 
+     //   Console.WriteLine("Введите два числа : ");
+ 
+    //    a = Convert.ToInt32(Console.ReadLine());
+ 
+       // b = Convert.ToInt32(Console.ReadLine());
+ 
+     //   if (a > b)
+ 
+       //   {
+ 
+       //       Console.WriteLine("{0} наибольшее число", a);
+ 
+      //    }
+ 
+      //    else
+ 
+        
+ 
+         //     Console.WriteLine("{0} наибольшее число ", b);
+      
+      //Вторая задача
+         int a, b, c;
+ 
+    //     Console.WriteLine("Введите три числа : ");
+ 
+    //     a = Convert.ToInt32(Console.ReadLine());
+ 
+    //     b = Convert.ToInt32(Console.ReadLine());
+ 
+    //     c = Convert.ToInt32(Console.ReadLine());
+    //     if (a > b) 
+   //      {
+ 
+   //          Console.WriteLine("{0} наибольшее число", a);
+ 
+    //     }
+ 
+   //      else
+ 
+   //      {
+ 
+  //           Console.WriteLine("{0} наибольшее число ", b);
+ //        }
+ //    if (c > a)
+  //   {
+ 
+  //           Console.WriteLine("{0} наибольшее число", c);
+ 
+  //       }
+ 
+   //      else
+ 
+   //      {
+ 
+   //          Console.WriteLine("{0} наибольшее число ", a);
+   //      }
+    //     if (c > b)
+    //      {
+ 
+    //         Console.WriteLine("{0} наибольшее число", c);
+ 
+    //     }
+ 
+   //      else
+ 
+    //     {
+ 
+   //          Console.WriteLine("{0} наибольшее число ", b);
+    //     }
+    // Третяя задача
+
+  //   int a;
+ 
+   //      Console.WriteLine("Введите  числo : ");
+ 
+    //     a = Convert.ToInt32(Console.ReadLine());
+
+    //     if (a%2 ==0)
+  // {
+ 
+   //          Console.WriteLine("Число четное");
+ 
+    //     }
+  //else 
+
+  //    {
+ 
+    //         Console.WriteLine("Число нечетное ");
+ 
+    //     }
+     // Четвертая задача
+  //    Console.WriteLine("Введите число n: ");
+  //     int n = Convert.ToInt32(Console.ReadLine());
+       
+  //     int num = 1;
+       
+   //    while (num < n){
+    //       if(num % 2 == 0){
+   //             Console.WriteLine(num);
+    //       }
+   //        num += 1;
+   //    }
